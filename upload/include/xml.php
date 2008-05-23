8fa5327394fc0b9f13731d9fe762baaa46a65404
Neal Poole <smartys@fluxbb.org>
2008-05-23 10:43:47 +0000

    Fixed an E_STRICT warning in include/xml.php, noticed by damaxxed.

--- upload/include/xml.php
+++ upload/include/xml.php
@@ -209,7 +209,8 @@ function validate_manifest($xml_array, $folder_name)
 					if (isset($hook['attributes']['priority']) && (!ctype_digit($hook['attributes']['priority']) || $hook['attributes']['priority'] < 0 || $hook['attributes']['priority'] > 10))
 						$errors[] = $lang_admin['extension/hooks/hook error3'];
 
-					$last_element = array_pop(token_get_all('<?php '.$hook['content']));
+					$tokenized_hook = token_get_all('<?php '.$hook['content']);
+					$last_element = array_pop($tokenized_hook);
 					if (is_array($last_element) && $last_element[0] == T_INLINE_HTML)
 						$errors[] = $lang_admin['extension/hooks/hook error4'];
 				}
