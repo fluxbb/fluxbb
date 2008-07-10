0f3c2ce2fd14680b333712d53c1f73233c023b13
Neal Poole <smartys@fluxbb.org>
2008-07-10 21:44:38 +0000

    Fixed an SMTP command injection vulnerability, discovered by Stefan Esser.

--- upload/include/email.php
+++ upload/include/email.php
@@ -123,6 +123,10 @@ function smtp_mail($to, $subject, $message, $headers = '')
 
 	$recipients = explode(',', $to);
 
+	// Sanitize the message
+	$message = str_replace("\r\n.", "\r\n..", $message);
+	$message = (substr($message, 0, 1) == '.' ? '.'.$message : $message);
+
 	// Are we using port 25 or a custom port?
 	if (strpos($pun_config['o_smtp_host'], ':') !== false)
 		list($smtp_host, $smtp_port) = explode(':', $pun_config['o_smtp_host']);
