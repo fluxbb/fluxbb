fe14ea6591ca689827c681293124fd03dc23723f
Paul Sullivan <paul@fluxbb.org>
2008-07-14 03:00:58 +0000

    And fixing the maintenance message a second time.

--- upload/include/functions.php
+++ upload/include/functions.php
@@ -2441,7 +2441,7 @@ function maintenance_message()
 	<div class="main-head">
 		<h1 class="hn"><span><?php echo $lang_common['Maintenance mode'] ?></span></h1>
 	</div>
-	<div class="main-content message">
+	<div class="main-content main-message">
 		<div class="content-box user-box">
 			<?php echo $message."\n" ?>
 		</div>
