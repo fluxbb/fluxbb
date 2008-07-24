9a97e3c0cc7e64534adb57f5a303b659fc3af6b4
Neal Poole <smartys@fluxbb.org>
2008-07-24 02:48:09 +0000

    Added phpDoc comments to the start of every FluxBB PHP file, replacing the existing GPL notice. This is the start of a movement toward using phpDoc-style comments more within the code.

--- upload/admin/users.php
+++ upload/admin/users.php
@@ -1,28 +1,14 @@
 <?php
-/***********************************************************************
-
-  Copyright (C) 2008  FluxBB.org
-
-  Based on code copyright (C) 2002-2008  PunBB.org
-
-  This file is part of FluxBB.
-
-  FluxBB is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published
-  by the Free Software Foundation; either version 2 of the License,
-  or (at your option) any later version.
-
-  FluxBB is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-  MA  02111-1307  USA
-
-************************************************************************/
+/**
+ * User search page
+ *
+ * Allows administrators or moderators to search the existing users based
+ * on various criteria.
+ *
+ * @copyright Copyright (C) 2008 FluxBB.org, based on code copyright (C) 2002-2008 PunBB.org
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package FluxBB
+ */
 
 
 if (!defined('FORUM_ROOT'))
