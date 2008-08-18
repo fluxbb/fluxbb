f7f29a60dc2a29e4f4f849532140bf3e3c30257e
Paul Sullivan <paul@fluxbb.org>
2008-08-18 17:54:39 +0000

    Item left out of previous commit.

--- upload/style/Oxygen/Oxygen.php
+++ upload/style/Oxygen/Oxygen.php
@@ -1,10 +1,3 @@
-<?php
-
-$style_config = array(
-'wide_post' => 'false'
-);
-
-?>
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Oxygen.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Oxygen_cs.css" />
 <!--[if lte IE 6]><link rel="stylesheet" type="text/css" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Oxygen_ie6.css" /><![endif]-->
