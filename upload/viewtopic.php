4a93a6f260ccb8c27a3e6c40ba85dbd7a7c68842
Quy Ton <quy@fluxbb.org>
2009-06-20 13:33:52 +0000

    Added check to suppress HTML if no avatar in viewtopic.php.

--- upload/viewtopic.php
+++ upload/viewtopic.php
@@ -331,7 +331,7 @@ while ($cur_post = $db->fetch_assoc($result))
 				<dl>
 					<dt><strong><?php echo $username ?></strong></dt>
 					<dd class="usertitle"><strong><?php echo $user_title ?></strong></dd>
-					<dd class="postavatar"><?php echo $user_avatar ?></dd>
+<?php if ($user_avatar != '') echo "\t\t\t\t\t".'<dd class="postavatar">'.$user_avatar.'</dd>'; ?>
 <?php if (count($user_info)) echo "\t\t\t\t\t".implode('</dd>'."\n\t\t\t\t\t", $user_info).'</dd>'."\n"; ?>
 <?php if (count($user_contacts)) echo "\t\t\t\t\t".'<dd class="usercontacts">'.implode('&nbsp;&nbsp;', $user_contacts).'</dd>'."\n"; ?>
 				</dl>
