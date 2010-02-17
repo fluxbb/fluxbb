bc81879745312646b4211669f10fc24cafd18e03
Jamie Furness <reines@fluxbb.org>
2010-02-17 12:17:15 +0000

    Its much easier to user is_numeric than regex

--- upload/admin_ranks.php
+++ upload/admin_ranks.php
@@ -33,7 +33,7 @@ if (isset($_POST['add_rank']))
 	if ($rank == '')
 		message($lang_admin_ranks['Must enter title']);
 
-	if (!@preg_match('#^\d+$#', $min_posts))
+	if (!is_numeric($min_posts))
 		message($lang_admin_ranks['Must be integer message']);
 
 	// Make sure there isn't already a rank with the same min_posts value
@@ -66,7 +66,7 @@ else if (isset($_POST['update']))
 	if ($rank == '')
 		message($lang_admin_ranks['Must enter title message']);
 
-	if (!@preg_match('#^\d+$#', $min_posts))
+	if (!is_numeric($min_posts))
 		message($lang_admin_ranks['Must be integer message']);
 
 	// Make sure there isn't already a rank with the same min_posts value
