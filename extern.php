4b97c3fb7f6ef47706108e4f29b5347bf6d94f40
Frank <frank@61924.nl>
2010-05-11 08:41:10 +0200

    Atom/RSS/XML feeds are now served with the right content-type header. Noticed by MattF.

--- extern.php
+++ extern.php
@@ -144,7 +144,7 @@ function output_atom($feed)
 	global $lang_common, $pun_config;
 
 	// Send XML/no cache headers
-	header('Content-Type: text/xml; charset=utf-8');
+	header('Content-Type: application/atom+xml; charset=utf-8');
 	header('Expires: '.gmdate('D, d M Y H:i:s').' GMT');
 	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 	header('Pragma: public');
