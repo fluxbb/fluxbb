2f74309093e405d37d8239319dcf948cb914c2ee
Frank <frank@61924.nl>
2010-07-18 10:38:58 +0200

    Russian and possibly other foreign characters couldn't be censored.

--- include/functions.php
+++ include/functions.php
@@ -741,7 +741,7 @@ function censor_words($text)
 		for ($i = 0; $i < $num_words; ++$i)
 		{
 			list($search_for[$i], $replace_with[$i]) = $db->fetch_row($result);
-			$search_for[$i] = '/\b('.str_replace('\*', '\w*?', preg_quote($search_for[$i], '/')).')\b/i';
+			$search_for[$i] = '/(?<=\W)('.str_replace('\*', '\w*?', preg_quote($search_for[$i], '/')).')(?=\W)/i';
 		}
 	}
 
