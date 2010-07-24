813ed59e28a0ca5f4d6229cc60ed7c66f475e963
Jamie Furness <jamie@jamierf.co.uk>
2010-07-24 15:46:01 +0100

    Splitting the query in userlist into 2 to improve performance of large boards.

--- userlist.php
+++ userlist.php
@@ -133,10 +133,18 @@ while ($cur_group = $db->fetch_assoc($result))
 			<tbody>
 <?php
 
-// Grab the users
-$result = $db->query('SELECT u.id, u.username, u.title, u.num_posts, u.registered, g.g_id, g.g_user_title FROM '.$db->prefix.'users AS u LEFT JOIN '.$db->prefix.'groups AS g ON g.g_id=u.group_id WHERE u.id>1 AND u.group_id!='.PUN_UNVERIFIED.(!empty($where_sql) ? ' AND '.implode(' AND ', $where_sql) : '').' ORDER BY '.$sort_by.' '.$sort_dir.', u.id ASC LIMIT '.$start_from.', 50') or error('Unable to fetch user list', __FILE__, __LINE__, $db->error());
+// Retrieve a list of user IDs, LIMIT is (really) expensive so we only fetch the IDs here then later fetch the remaining data
+$result = $db->query('SELECT u.id FROM '.$db->prefix.'users AS u WHERE u.id>1 AND u.group_id!='.PUN_UNVERIFIED.(!empty($where_sql) ? ' AND '.implode(' AND ', $where_sql) : '').' ORDER BY '.$sort_by.' '.$sort_dir.', u.id ASC LIMIT '.$start_from.', 50') or error('Unable to fetch user IDs', __FILE__, __LINE__, $db->error());
+
 if ($db->num_rows($result))
 {
+	$user_ids = array();
+	for ($i = 0;$cur_user_id = $db->result($result, $i);$i++)
+		$user_ids[] = $cur_user_id;
+
+	// Grab the users
+	$result = $db->query('SELECT u.id, u.username, u.title, u.num_posts, u.registered, g.g_id, g.g_user_title FROM '.$db->prefix.'users AS u LEFT JOIN '.$db->prefix.'groups AS g ON g.g_id=u.group_id WHERE u.id IN('.implode(',', $user_ids).') ORDER BY '.$sort_by.' '.$sort_dir.', u.id ASC') or error('Unable to fetch user list', __FILE__, __LINE__, $db->error());
+
 	while ($user_data = $db->fetch_assoc($result))
 	{
 		$user_title_field = get_title($user_data);
