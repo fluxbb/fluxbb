3de5a0d1edc46569aa86cf8ced3caaad1626ddb3
Jamie Furness <jamie@jamierf.co.uk>
2010-07-28 10:57:42 +0100

    Revert "Fixing missing LOWER in query in admin_users"
    This reverts commit e2688edabaf2e5aec21726644d5757b64213cf18.
    
    Conflicts:
    
    	admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -325,11 +325,7 @@ else if (isset($_GET['find_user']))
 	{
 		if ($input != '' && in_array($key, array('username', 'email', 'title', 'realname', 'url', 'jabber', 'icq', 'msn', 'aim', 'yahoo', 'location', 'signature', 'admin_note')))
 		{
-<<<<<<< HEAD
-			$conditions[] = 'LOWER(u.'.$db->escape($key).') LIKE LOWER(\''.$db->escape(str_replace('*', '%', $input)).'\')';
-=======
 			$conditions[] = 'u.'.$db->escape($key).' '.$like_command.' \''.$db->escape(str_replace('*', '%', $input)).'\'';
->>>>>>> 158ad27... Updating some places to perform case insensitive search
 			$query_str[] = 'form%5B'.$key.'%5D='.urlencode($input);
 		}
 	}
