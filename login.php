9d53b673c60f617efae3f2a222043624a6aa7db7
Jamie Furness <jamie@jamierf.co.uk>
2011-01-28 15:47:47 +0000

    Ensure the path component exists before using it when validating a referrer URL.

--- login.php
+++ login.php
@@ -230,10 +230,18 @@ if (!empty($_SERVER['HTTP_REFERER']))
 	if (strpos($referrer['host'], 'www.') === 0)
 		$referrer['host'] = substr($referrer['host'], 4);
 
+	// Make sure the path component exists
+	if (!isset($referrer['path']))
+		$referrer['path'] = '';
+
 	$valid = parse_url(get_base_url());
 	// Remove www subdomain if it exists
 	if (strpos($valid['host'], 'www.') === 0)
 		$valid['host'] = substr($valid['host'], 4);
+
+	// Make sure the path component exists
+	if (!isset($valid['path']))
+		$valid['path'] = '';
 
 	if ($referrer['host'] == $valid['host'] && preg_match('#^'.preg_quote($valid['path']).'/(.*?)\.php#i', $referrer['path']))
 		$redirect_url = $_SERVER['HTTP_REFERER'];
