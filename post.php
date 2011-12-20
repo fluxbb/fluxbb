0d35b370c2618fd06037c6183d7da3f39e0741ed
Franz Liedke <franz@develophp.org>
2011-12-21 00:36:38 +0100

    #485: Replace four-byte characters in posts (as MySQL cannot handle them properly).

--- post.php
+++ post.php
@@ -152,6 +152,9 @@ if (isset($_POST['form_sent']))
 	$hide_smilies = isset($_POST['hide_smilies']) ? '1' : '0';
 	$subscribe = isset($_POST['subscribe']) ? '1' : '0';
 	$stick_topic = isset($_POST['stick_topic']) && $is_admmod ? '1' : '0';
+	
+	// Replace four-byte characters (MySQL cannot handle them)
+	$message = strip_bad_multibyte_chars($message);
 
 	$now = time();
 
