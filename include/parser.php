12c6c038a5b2dd8c29563feffb1118427245bce7
Jamie Furness <jamie@jamierf.co.uk>
2012-01-01 13:58:18 +0000

    Fixing paragraph regex in the parser to be possessive. #508

--- include/parser.php
+++ include/parser.php
@@ -885,7 +885,8 @@ function parse_message($text, $hide_smilies)
 	}
 
 	// Add paragraph tag around post, but make sure there are no empty paragraphs
-	$text = preg_replace('%<br />\s*?<br />((\s*<br />)*)%i', "</p>$1<p>", $text);
+	$text = preg_replace('%<br />\s*?<br />((\s*<br />)*+)%i', "</p>$1<p>", $text);
+
 	$text = str_replace('<p><br />', '<p>', $text);
 	$text = str_replace('<p></p>', '', '<p>'.$text.'</p>');
 
