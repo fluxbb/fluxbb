700d52eea07bf0e30469432854efb9ea8fb5486a
Franz Liedke <franz@develophp.org>
2012-05-03 10:37:10 +0200

    Change version number to 1.4.9.

--- install.php
+++ install.php
@@ -7,7 +7,7 @@
  */
 
 // The FluxBB version this script installs
-define('FORUM_VERSION', '1.4.8');
+define('FORUM_VERSION', '1.4.9');
 
 define('FORUM_DB_REVISION', 15);
 define('FORUM_SI_REVISION', 2);
