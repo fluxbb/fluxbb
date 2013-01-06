02af9b412601a066c993c82392d1241611a4b5f0
Franz Liedke <franz@develophp.org>
2013-01-07 00:23:57 +0100

    Add mail configuration file.

---
+++ config/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'host' => '',
+	'port' => 2525,
+	'from' => array('address' => null, 'name' => null),
+	'encryption' => 'tls',
+	'username' => null,
+	'password' => null,
+
+);
