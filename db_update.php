1b5763db4a75010252db9ca566a49f59ac0d91f3
Franz Liedke <franz@develophp.org>
2013-02-22 12:15:11 +0100

    Tag v1.5.3.

--- db_update.php
+++ db_update.php
@@ -7,7 +7,7 @@
  */
 
 // The FluxBB version this script updates to
-define('UPDATE_TO', '1.5.2');
+define('UPDATE_TO', '1.5.3');
 
 define('UPDATE_TO_DB_REVISION', 18);
 define('UPDATE_TO_SI_REVISION', 2);
--- include/common.php
+++ include/common.php
@@ -10,7 +10,7 @@ if (!defined('PUN_ROOT'))
 	exit('The constant PUN_ROOT must be defined and point to a valid FluxBB installation root directory.');
 
 // Define the version and database revision that this code was written for
-define('FORUM_VERSION', '1.5.2');
+define('FORUM_VERSION', '1.5.3');
 
 define('FORUM_DB_REVISION', 18);
 define('FORUM_SI_REVISION', 2);
