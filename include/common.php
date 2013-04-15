197b46a69474108bee5eed2e9f8f2b94af913496
Yannick <contact@studio384.be>
2013-04-15 20:02:45 +0200

    #838 Database revision in common.php to 19

--- include/common.php
+++ include/common.php
@@ -12,7 +12,7 @@ if (!defined('PUN_ROOT'))
 // Define the version and database revision that this code was written for
 define('FORUM_VERSION', '1.5.3');
 
-define('FORUM_DB_REVISION', 18);
+define('FORUM_DB_REVISION', 19);
 define('FORUM_SI_REVISION', 2);
 define('FORUM_PARSER_REVISION', 2);
 
