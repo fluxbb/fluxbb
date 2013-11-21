0dd8a6d79c67d312e982ac71bde4a78fd5774ba4
Franz Liedke <franz@develophp.org>
2013-11-21 15:11:33 +0100

    #906: Implement SQLite3 driver.

--- include/dblayer/common_db.php
+++ include/dblayer/common_db.php
@@ -38,6 +38,10 @@ switch ($db_type)
 		require_once PUN_ROOT.'include/dblayer/sqlite.php';
 		break;
 
+	case 'sqlite3':
+		require_once PUN_ROOT.'include/dblayer/sqlite3.php';
+		break;
+
 	default:
 		error('\''.$db_type.'\' is not a valid database type. Please check settings in config.php.', __FILE__, __LINE__);
 		break;
