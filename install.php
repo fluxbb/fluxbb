69bb1f08e246f3b458805758cdd18ce914850f01
Charlie Merland <charlie.merland@gmail.com>
2014-01-14 16:32:40 +0100

    DB Revision + typo consistency

--- install.php
+++ install.php
@@ -9,7 +9,7 @@
 // The FluxBB version this script installs
 define('FORUM_VERSION', '1.5.6');
 
-define('FORUM_DB_REVISION', 20);
+define('FORUM_DB_REVISION', 21);
 define('FORUM_SI_REVISION', 2);
 define('FORUM_PARSER_REVISION', 2);
 
