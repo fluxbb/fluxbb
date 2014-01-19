56e69875d58d4989d86f6e3296d77000725921d5
Franz Liedke <franz@develophp.org>
2014-01-19 02:25:50 +0100

    Move config and start directory to project root.

--- start/global.php
+++ start/global.php
@@ -66,16 +66,3 @@ App::down(function()
 {
 	return Response::make("Be right back!", 503);
 });
-
-/*
-|--------------------------------------------------------------------------
-| Require The Filters File
-|--------------------------------------------------------------------------
-|
-| Next we will load the filters file for the application. This gives us
-| a nice separate location to store our route and application filter
-| definitions instead of putting them all in the main routes file.
-|
-*/
-
-require app_path().'/filters.php';
