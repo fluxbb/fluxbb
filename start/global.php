77ca338a36c45a2a750f0dc10c8ccf11c787a8c4
Franz Liedke <franz@develophp.org>
2014-01-19 13:18:32 +0100

    Delete some autoloaded directories.

--- start/global.php
+++ start/global.php
@@ -13,10 +13,7 @@
 
 ClassLoader::addDirectories(array(
 
-	app_path().'/commands',
-	app_path().'/controllers',
-	app_path().'/models',
-	app_path().'/database/seeds',
+	// app_path().'/src',
 
 ));
 
