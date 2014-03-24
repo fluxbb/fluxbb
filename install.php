f794b73063c6c30eb4323590b4e4ffaf6b13935f
Yannick <contact@studio384.be>
2014-03-24 20:18:27 +0100

    #948 Require passwords with at least 6 characters

--- install.php
+++ install.php
@@ -184,7 +184,7 @@ else
 	else if (preg_match('%(?:\[/?(?:b|u|i|h|colou?r|quote|code|img|url|email|list)\]|\[(?:code|quote|list)=)%i', $username))
 		$alerts[] = $lang_install['Username 6'];
 
-	if (pun_strlen($password1) < 4)
+	if (pun_strlen($password1) < 6)
 		$alerts[] = $lang_install['Short password'];
 	else if ($password1 != $password2)
 		$alerts[] = $lang_install['Passwords not match'];
