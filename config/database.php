2fd9d1a99e14c2e0367f7249470d009bca09a431
Franz Liedke <franz@develophp.org>
2014-06-23 12:36:20 +0200

    Change default database config to Homestead settings.

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'homestead',
+			'username'  => 'homestead',
+			'password'  => 'secret',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
