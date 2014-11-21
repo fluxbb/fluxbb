d66558195d4b9ea38bc4de37c9f098f2786af6f7
Franz Liedke <franz@develophp.org>
2014-11-21 13:11:04 +0100

    #997: Increase length of random passwords.

--- register.php
+++ register.php
@@ -79,7 +79,7 @@ if (isset($_POST['form_sent']))
 	{
 		$email2 = strtolower(pun_trim($_POST['req_email2']));
 
-		$password1 = random_pass(8);
+		$password1 = random_pass(12);
 		$password2 = $password1;
 	}
 	else
