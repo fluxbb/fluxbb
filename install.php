8b7e051c8a0cf4a027180ef4e3bc0c223c6237cb
Eric Hocking <xaoseric@users.noreply.github.com>
2015-03-25 13:43:13 -0400

    Dropping php 4 support
    It is safe to assume that most people are on php 5, so lets drop support for php 4.

--- install.php
+++ install.php
@@ -7,13 +7,13 @@
  */
 
 // The FluxBB version this script installs
-define('FORUM_VERSION', '1.5.7');
+define('FORUM_VERSION', '1.5.9');
 
 define('FORUM_DB_REVISION', 21);
 define('FORUM_SI_REVISION', 2);
 define('FORUM_PARSER_REVISION', 2);
 
-define('MIN_PHP_VERSION', '4.4.0');
+define('MIN_PHP_VERSION', '5.0.0');
 define('MIN_MYSQL_VERSION', '4.1.2');
 define('MIN_PGSQL_VERSION', '7.0.0');
 define('PUN_SEARCH_MIN_WORD', 3);
