3909815c00f859fd565ca9e7bdcd254dba23c585
Franz Liedke <franz@develophp.org>
2018-12-31 03:25:49 +0100

    Bump version numbers to 1.5.11

--- db_update.php
+++ db_update.php
@@ -7,7 +7,7 @@
  */
 
 // The FluxBB version this script updates to
-define('UPDATE_TO', '1.5.10');
+define('UPDATE_TO', '1.5.11');
 
 define('UPDATE_TO_DB_REVISION', 21);
 define('UPDATE_TO_SI_REVISION', 2);
