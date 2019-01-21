1730eeff9542ecc8c667b924850c282f858e046f
Quy <quy@fluxbb.org>
2019-01-21 08:57:01 -0800

    Bump db revision #

--- install.php
+++ install.php
@@ -9,7 +9,7 @@
 // The FluxBB version this script installs
 define('FORUM_VERSION', '1.5.11');
 
-define('FORUM_DB_REVISION', 23);
+define('FORUM_DB_REVISION', 24);
 define('FORUM_SI_REVISION', 2);
 define('FORUM_PARSER_REVISION', 2);
 
