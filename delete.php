53a1913d4542bcec6ce50e16513f77ec99986453
Quy <quy@fluxbb.org>
2019-02-20 14:11:25 -0800

    Fix comma in wrong array

--- delete.php
+++ delete.php
@@ -82,8 +82,8 @@ $crumbs = generate_crumbs(array(
 	array($lang_common['Index'], 'index.php'),
 	array($cur_post['forum_name'], 'viewforum.php?id='.$cur_post['fid']),
 	array($cur_post['subject'], 'viewtopic.php?pid='.$id.'#p'.$id),
-	$lang_delete['Delete post']),
-);
+	$lang_delete['Delete post'],
+));
 
 $page_title = array(pun_htmlspecialchars($pun_config['o_board_title']), $lang_delete['Delete post']);
 define ('PUN_ACTIVE_PAGE', 'index');
