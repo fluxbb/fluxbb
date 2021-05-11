5a2f95a34ef426f2b12103a32d5d4ba24a14391a
Quy <quy@fluxbb.org>
2021-05-11 15:16:29 -0700

    Apply flux_password_hash to new password (#235)
    #1147 The new password got saved using the wrong hash, thus logging in with the new password would fail.

--- login.php
+++ login.php
@@ -165,7 +165,7 @@ else if ($action == 'forget' || $action == 'forget_2')
 					$new_password = random_pass(12);
 					$new_password_key = random_pass(8);
 
-					$db->query('UPDATE '.$db->prefix.'users SET activate_string=\''.pun_hash($new_password).'\', activate_key=\''.$new_password_key.'\', last_email_sent = '.time().' WHERE id='.$cur_hit['id']) or error('Unable to update activation data', __FILE__, __LINE__, $db->error());
+					$db->query('UPDATE '.$db->prefix.'users SET activate_string=\''.flux_password_hash($new_password).'\', activate_key=\''.$new_password_key.'\', last_email_sent = '.time().' WHERE id='.$cur_hit['id']) or error('Unable to update activation data', __FILE__, __LINE__, $db->error());
 
 					// Do the user specific replacements to the template
 					$cur_mail_message = str_replace('<username>', $cur_hit['username'], $mail_message);
