a31254f656b00b417e7a2ae9febe7193ba636e04
Motify <93656397+mtfy@users.noreply.github.com>
2021-12-03 20:29:10 +0200

    Add files via upload

---
+++ lang/Finnish/payment.php
@@ -0,0 +1,15 @@
+<?php
+
+// Language definitions used in payment.php, newpayment.php, payment_query.php and motify_payments.php
+$lang_payment = array(
+    'PayPal 1'			    =>	'',
+    'PayPal 2'              =>  '',
+    'PayPal 3'              =>  '',
+    'PayPal 4'              =>  '',
+    'PayPal 5'              =>  '',
+    'Plan'                  =>  '',
+    'Transaction ID'        =>  '',
+    'Billing Email'         =>  '',
+    'Submit'                =>  '',
+    'Cancel'                =>  ''
+);
