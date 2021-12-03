f67736f3527e5a16f6eea0cd0fb79a72c1f28bb1
Motify <93656397+mtfy@users.noreply.github.com>
2021-12-03 22:13:41 +0000

    Update payment.php

--- lang/English/payment.php
+++ lang/English/payment.php
@@ -2,7 +2,7 @@
 
 // Language definitions used in payment.php, newpayment.php, payment_query.php and motify_payments.php
 $lang_payment = array(
-    'PayPal 1'			    =>	'Sign in to your PayPal account using your web browser. Click %sSend &amp; Request%s at top of the page.',
+    'PayPal 1'			    =>	'Sign in to your PayPal account using your web browser. Click %sSend &amp; Request%s at top of the page. Enter the email address as specified below to the recipient address.',
     'PayPal 2'              =>  'Enter %s for the amount, EUR for the currency, and enter the phrase below for the note and click %sContinue%s.',
     'PayPal 3'              =>  'Now, select the above option %sSending to a friend%s, when prompted.',
     'PayPal 4'              =>  'You must send the money using the %sFriends &amp; Family%s option! All other payments than &apos;Friends &amp; Family&apos; will result your account being permamently banned!',
