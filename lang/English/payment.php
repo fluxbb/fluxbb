f787a12f7776f19053ba2f556efe66f1b21f586b
Motify <93656397+mtfy@users.noreply.github.com>
2021-12-03 13:50:00 +0000

    Create translation for payment.php

---
+++ lang/English/payment.php
@@ -0,0 +1,15 @@
+<?php
+
+// Language definitions used in payment.php, newpayment.php, payment_query.php and motify_payments.php
+$lang_payment = array(
+    'PayPal 1'			    =>	'Sign in to your PayPal account using your web browser. Click %sSend &amp; Request%s at the top of submit Transaction ID, and email address associated to your PayPal account e page. In the textbox, type in the email address shown below.',
+    'PayPal 2'              =>  'Enter %s for the amount, EUR for the currency, and enter the phrase below for the note and click %sContinue%s.',
+    'PayPal 3'              =>  'Now, select the above option %sSending to a friend%s, if prompted.',
+    'PayPal 4'              =>  'You must send the money using the %sFriends &amp; Family%s option! All other payments than &apos;Friends &amp; Family&aåps; will result in a ban!',
+    'PayPal 5'              =>  'Complete the payment. Make sure to submit Transaction ID, and your email address associated to your PayPal account below. An administrator will review your payment manually. This process may take up to 24 hours, so please be patient!'
+    'Plan'                  =>  '%s for %s days',
+    'Transaction ID'        =>  'Transaction ID',
+    'Billing Email'         =>  'Blling Email',
+    'Submit'                =>  'Submit',
+    'Cancel'                =>  'Cancel'
+);
