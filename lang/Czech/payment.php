cbcc6e6acc252b75024476058b6667232b84f22d
Motify <93656397+mtfy@users.noreply.github.com>
2021-12-07 22:56:53 +0200

    Add files via upload

---
+++ lang/Czech/payment.php
@@ -0,0 +1,25 @@
+<?php
+
+// Language definitions used in payment.php, newpayment.php, payment_query.php and motify_payments.php
+$lang_payment = array(
+    'PayPal 1'			    =>	'',
+    'PayPal 2'              =>  '',
+    'PayPal 3'              =>  '',
+    'PayPal 4'              =>  '',
+    'PayPal 5'              =>  '',
+    'Plan'                  =>  '',
+    'Transaction ID'        =>  '',
+    'Billing Email'         =>  '',
+	'Billing Email Tooltip' =>  '',
+    'Submit'                =>  '',
+    'Cancel'                =>  '',
+	'501'                   =>  '',
+    '403'                   =>  '',
+    '400'                   =>  '',
+	'429'          			=>  '',
+    'Invoice confirmation'  =>  '',
+    'Success'               =>  '',
+    'Success Text'          =>  '',
+    'Failed' 				=>  '',
+    'Awaiting Approval'     =>  '',
+  );
